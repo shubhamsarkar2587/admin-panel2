08c6d34c28300b9769ac8283b90d45bf016ffb8a
shubham <shubhamsarkar2587@gmail.com>
2023-01-09 11:24:57 +0530

    Title: fixed bugs in dashboard

--- src/assets/asset.js
+++ src/assets/asset.js
@@ -3,11 +3,12 @@ import login_screen_img from './svg/login_screen_img.svg'
 import login_enter from './svg/login_enter.svg'
 import show_password from './svg/show_password.svg'
 import hide_password from './svg/hide_password.svg'
-
 import kyc from './svg/kyc.svg'
 import rightArrow from './svg/rightArrow.svg'
 import defaultUser from './svg/defaultUser.svg';
 import notification from './svg/notification.svg';
+import application from './svg/application.svg'
+import graph from './svg/graph.svg'
 
 export const pngAssets = {
   bigul
@@ -27,6 +28,8 @@ export const svgAssets = {
   },
   dashboard: {
     kyc,
-    rightArrow
+    rightArrow,
+    application,
+    graph
   }
 };
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,6 +1,8 @@
 import Navbar from "../components/navbar/Navbar";
 import Sidebar from "../components/sidebar/Sidebar";
 import DisplayTable from "../components/reactTable/displayTable";
+import Applications from "../components/widgets/Applications";
+import Date from "../components/widgets/Date";
 
 const Homepage = () => {
   return (
@@ -8,8 +10,20 @@ const Homepage = () => {
       <Navbar />
       <div className="flex">
         <Sidebar />
-        <div className="pl-5 pr-[30px] py-5 flex grow w-full h-full">
-          <DisplayTable />
+        <div className="pl-5 pr-[30px] py-5 flex flex-col grow w-full h-full">
+          <div className="mb-[30px] flex">
+            <Date />
+            <Date />
+            <Date />
+          </div>
+          <div className="mb-[30px] grid grid-cols-3 gap-x-9">
+            <Applications />
+            <Applications />
+            <Applications />
+          </div>
+          <div>
+            <DisplayTable />
+          </div>
         </div>
       </div>
     </>
