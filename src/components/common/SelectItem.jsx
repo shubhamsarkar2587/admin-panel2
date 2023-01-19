0ae9df14427826e180069bb575d81547048e238b
shubham <shubhamsarkar2587@gmail.com>
2023-01-19 21:14:07 +0530

    Fixed border in select input

--- src/components/common/SelectItem.jsx
+++ src/components/common/SelectItem.jsx
@@ -27,7 +27,7 @@ const SelectItem = ({ height, placeholder }) => {
       }}
       options={selectOption}
       bordered={false}
-      className={`flex items-center text-[#323232] border border-solid border-[#DFDFDF] rounded-[10px] shadow-[0px_2px_10px_rgba(201,201,201,0.25)] bg-white`}
+      className={`flex items-center text-[#323232] rounded-[10px] shadow-[0px_2px_10px_rgba(201,201,201,0.25)] bg-white`}
     />
   )
 }
