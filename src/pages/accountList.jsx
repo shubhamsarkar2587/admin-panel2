24da93d2a2489e8f766ea70db83ab50119570dc2
shubham <shubhamsarkar2587@gmail.com>
2023-02-06 20:12:20 +0530

    Fixed account list

--- src/pages/accountList.jsx
+++ src/pages/accountList.jsx
@@ -1,30 +1,57 @@
 import { ViewAllBtn } from '../components/buttons/ViewAllBtn';
 import DatePickerInput from '../components/common/DatePicker';
+import { MyPopover } from '../components/popover/Popover';
 import { Pagination } from '../components/reactTable/Pagination';
 import { ReactTable } from '../components/reactTable/ReactTable';
+import { reactTableStatusValue } from '../components/reactTable/reactTableData';
 import { ReactTableHeader } from '../components/reactTable/ReactTableHeader';
+import { PopoverChildComp, PopoverParentComp } from '../components/reactTable/ReactTablePopupBtn';
 import { SearchBar } from '../components/searchbar/SearchBar';
 
-const columns = [
-	{ Header: 'Client Name', accessor: 'clientName' },
-	{ Header: 'Pan', accessor: 'pan' },
-	{ Header: 'Mobile Number', accessor: 'mobileNumber' },
-	{ Header: 'Steps', accessor: 'steps' },
-	{ Header: 'Created At', accessor: 'createdAt' },
-	{ Header: 'Updated At', accessor: 'updatedAt' },
-	{ Header: 'Actions', accessor: 'actions' }
-];
-
 const data = [
-	{ clientName: 'Tony Stark', pan: 34342232334, mobileNumber: 'fdf3343', steps: 'dfdfdf0', createdAt: 'sdsd', updatedAt: 'ddfdf', actions: 'dd' },
-	{ clientName: 'Tony Stark', pan: 34342232334, mobileNumber: 'fdf3343', steps: 'dfdfdf0', createdAt: 'sdsd', updatedAt: 'ddfdf', actions: 'dd' },
-	{ clientName: 'Tony Stark', pan: 34342232334, mobileNumber: 'fdf3343', steps: 'dfdfdf0', createdAt: 'sdsd', updatedAt: 'ddfdf', actions: 'dd' },
-	{ clientName: 'Tony Stark', pan: 34342232334, mobileNumber: 'fdf3343', steps: 'dfdfdf0', createdAt: 'sdsd', updatedAt: 'ddfdf', actions: 'dd' },
-	{ clientName: 'Tony Stark', pan: 34342232334, mobileNumber: 'fdf3343', steps: 'dfdfdf0', createdAt: 'sdsd', updatedAt: 'ddfdf', actions: 'dd' },
-	{ clientName: 'Tony Stark', pan: 34342232334, mobileNumber: 'fdf3343', steps: 'dfdfdf0', createdAt: 'sdsd', updatedAt: 'ddfdf', actions: 'dd' }
+	{ customerName: 'Ankit Singh', ldCode: 'BHK70128GT', kycSource: '', rmName: 'V.K. Singh', verifier: 'M.K. Bansal', teleCallerName: 'Sumit Soni', acCreatedDate: '16 Dec 2022', acStatus: 'Active', actions: '' },
+	{ customerName: 'Ankit Singh', ldCode: 'BHK70128GT', kycSource: '', rmName: 'V.K. Singh', verifier: 'M.K. Bansal', teleCallerName: 'Sumit Soni', acCreatedDate: '16 Dec 2022', acStatus: 'InActive', actions: '' },
+	{ customerName: 'Ankit Singh', ldCode: 'BHK70128GT', kycSource: '', rmName: 'V.K. Singh', verifier: 'M.K. Bansal', teleCallerName: 'Sumit Soni', acCreatedDate: '16 Dec 2022', acStatus: 'Closed', actions: '' },
+	{ customerName: 'Ankit Singh', ldCode: 'BHK70128GT', kycSource: '', rmName: 'V.K. Singh', verifier: 'M.K. Bansal', teleCallerName: 'Sumit Soni', acCreatedDate: '16 Dec 2022', acStatus: 'Dormant', actions: '' },
+	{ customerName: 'Ankit Singh', ldCode: 'BHK70128GT', kycSource: '', rmName: 'V.K. Singh', verifier: 'M.K. Bansal', teleCallerName: 'Sumit Soni', acCreatedDate: '16 Dec 2022', acStatus: 'Suspended', actions: '' }
 ];
 
 export const AccountList = () => {
+	const columns = [
+		{ Header: 'Customer Name', accessor: 'customerName', minWidth: 130 },
+		{ Header: 'LD Code', accessor: 'ldCode', minWidth: 130 },
+		{ Header: 'KYC Source', accessor: 'kycSource', minWidth: 130 },
+		{ Header: 'RM Name', accessor: 'rmName', minWidth: 130 },
+		{ Header: 'Verifier', accessor: 'verifier', minWidth: 130 },
+		{ Header: 'Telecaller Name', accessor: 'teleCallerName', minWidth: 130 },
+		{ Header: 'A/C Created Date', accessor: 'acCreatedDate', minWidth: 130 },
+		{
+			Header: 'A/C Created Date',
+			accessor: 'acStatus',
+			minWidth: 130,
+			Cell: ({ row }) => (
+				<div
+					className="min-w-[95px] px-3 py-1 inline-flex justify-center rounded-[6px] text-xs font-medium font-poppinsMedium"
+					style={{
+						backgroundColor: reactTableStatusValue[row.original.acStatus]?.bgColor
+					}}
+				>
+					{row.original.acStatus}
+				</div>
+			)
+		},
+		{
+			Header: 'Actions',
+			accessor: 'actions',
+			minWidth: 60,
+			Cell: ({ row }) => (
+				<MyPopover
+					PopoverParentComp={PopoverParentComp}
+					PopoverChildComp={PopoverChildComp}
+				/>
+			)
+		}
+	];
 	return (
 		<>
 			<div className="w-full flex flex-col ">
