c575c98cbf3fffffc3a257dee34159375ac6d0fe
shubham <shubhamsarkar9654@gmail.com>
2023-02-17 16:13:54 +0530

    Fixed search bar icon

--- src/components/searchbar/SearchBar.jsx
+++ src/components/searchbar/SearchBar.jsx
@@ -47,8 +47,8 @@ export const SearchBar = ({ height, placeholder, border }) => {
 						value={searchText}
 						onChange={(e) => handleSearch({ value: e.target.value })}
 					/>
-					<svg className="h-4 w-4 fill-current text-[#A3A3A3]" viewBox="0 0 20 20">
-						<path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
+					<svg className="h-4 w-4 fill-current" viewBox="0 0 20 20">
+						<path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" fill='black' />
 					</svg>
 				</div>
 				<div
